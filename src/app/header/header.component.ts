69e81460111a9c4ec12f0a721aed0886543d6dce
codercrocs <rajkumarakonfans@gmail.com>
2024-01-04 11:35:11 +0530

    updated the codes for better user experience

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-
+import { ThemeServiceService } from '../theme-service.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private themeservice: ThemeServiceService) {}
+  theme = true;
+  toggle() {
+    this.theme = this.theme === true ? false : true;
+    this.themeservice.toggleTheme();
+  }
+}
